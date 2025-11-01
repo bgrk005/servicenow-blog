fadd5d2d0c4497e872b333c2804f174127092541
Gangadhar Ravi Bandaru <gangadharbandaru005@gmail.com>
2025-11-01 10:06:39 -0400

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   // Replace this with your deployed site URL.  It is used when generating
   // canonical links, sitemaps and the RSS feed.
-  site: 'https://<your-username>.github.io/<your-repo>',
+  site: 'https://bgrk005.github.io/servicenow-blog/',
   integrations: [
     tailwind(),
     sitemap(),
